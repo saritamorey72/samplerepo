8838ed1db060f460c5945e7a77450a3656f6b9a9
mosaic@sc.com <mosaic@sc.com>
2021-08-09 10:05:07 +0000

    111 auto save by knights-watch

--- rcode.R
+++ rcode.R
@@ -4,4 +4,12 @@ fileConn<-file("/data/output12.txt")
 writeLines(c("Hello","World"), fileConn)
 close(fileConn)
 
+fileConn<-file("/test_mosaic/G2/output12.txt")
+writeLines(c("Hello","World"), fileConn)
+close(fileConn)
+
+fileConn<-file("/test_mosaic/G1/output12.txt")
+writeLines(c("Hello","World"), fileConn)
+close(fileConn)
+
 
