2bc3306cc44cdbc98f720f6746a404cacda3fcb3
aman <aman>
2021-08-12 07:10:04 +0000

    2 auto save by knights-watch

--- R_user_1/user1_read_G2.R
+++ R_user_1/user1_read_G2.R
@@ -1,3 +1,3 @@
-fileConn<-file("/test_mosaic/G3/data_writee.txt")
-readLines(("/test_mosaic/G3/data_writee1.txt"), fileConn)
+fileConn<-file("/test_mosaic/G2/data_writee.txt")
+readLines(("/test_mosaic/G2/data_writee1.txt"), fileConn)
 close(fileConn)
