9d0b2cefd4c68e2a7aecc70bf5a92778d371443e
mosaic@sc.com <mosaic@sc.com>
2021-08-12 08:16:13 +0000

    2 auto save by knights-watch

--- R_user_1/user1_G1_read.R
+++ R_user_1/user1_G1_read.R
@@ -1,3 +1,3 @@
-fileConn<-file("/test_mosaic/G1/writeG1.txt")
-readLines(("/test_mosaic/G1/writeG1.txt"), fileConn)
+fileConn<-file("/test_mosaic/G3/writeG1.txt")
+readLines(("/test_mosaic/G3/writeG1.txt"), fileConn)
 close(fileConn)
